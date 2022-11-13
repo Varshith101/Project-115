fffab0c18d0abb606bda2f9eb5b34371d9e8c758
Varshith101 <86972437+Varshith101@users.noreply.github.com>
2022-11-13 10:37:06 -0600

    Add files via upload

---
+++ file_system_move_File.py
@@ -0,0 +1,8 @@
+import os
+
+source = 'main.txt'
+
+dest = 'newfile.txt'
+
+from_dir = "C:\\Users\\User\\Downloads\\Class 115"            
+to_dir = "C:\\Users\\User\Downloads\\C115-teacherreference-main"
